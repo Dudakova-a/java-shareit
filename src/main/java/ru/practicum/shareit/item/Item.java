3332aafad2a2909296fcfdc2238249f1110f88a7
Anastasiya <dunas1@yandex.ru>
2025-09-16 14:22:02 +0800

    попытка №5

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.practicum.shareit.request.ItemRequest;
-import ru.practicum.shareit.user.User;
+
 
 @Data
 @NoArgsConstructor
