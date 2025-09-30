1e8523777b46d4528af6d0c46278362b2434ff96
Anastasiya <dunas1@yandex.ru>
2025-09-30 13:31:02 +0800

    выполнено согласно ФЗ15 еще раз

--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * Реализация сервиса запросов вещей.
+ * Реализация сервиса запросов вещей
  */
 @Service
 @RequiredArgsConstructor
