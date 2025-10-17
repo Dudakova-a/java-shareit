52367a3cc1b9a4ee6a12ca08e567461971d80bb8
Anastasiya <dunas1@yandex.ru>
2025-10-17 10:38:50 +0800

    попытка 1

--- gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/dto/BookingCreateDto.java
@@ -3,7 +3,6 @@ package ru.practicum.shareit.booking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
